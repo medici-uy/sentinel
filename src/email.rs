c41fdeafef15173b2bf978efa73c86a1242c2e62
Diego Stratta <18295954+dbstratta@users.noreply.github.com>
2023-11-06 14:53:19 -0300

    not send email

--- src/email.rs
+++ src/email.rs
@@ -4,7 +4,7 @@ use super::Status;
 use super::AWS_SES_CLIENT;
 
 pub async fn send_email(status: Status) -> Result<(), Error> {
-    AWS_SES_CLIENT.send_email().send().await?;
+    // AWS_SES_CLIENT.send_email().send().await?;
 
     Ok(())
 }
