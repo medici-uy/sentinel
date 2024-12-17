ed916c25f4dd7de6ced39d1473f1af05e0b1efc5
Diego Stratta <18295954+dbstratta@users.noreply.github.com>
2024-12-17 16:01:20 -0300

    fix payload type

--- src/main.rs
+++ src/main.rs
@@ -4,17 +4,25 @@ mod helpers;
 mod status;
 
 use lambda_runtime::{service_fn, Error, LambdaEvent};
+use serde::Deserialize;
 
 use config::CONFIG;
 use email::send_email;
 use helpers::insert_status_change;
 use status::Status;
 
-async fn handler(_event: LambdaEvent<()>) -> Result<(), Error> {
+#[derive(Deserialize)]
+struct Payload {}
+
+async fn handler(_event: LambdaEvent<Payload>) -> Result<(), Error> {
+    println!("1");
     let status = Status::check().await;
+    println!("2");
 
     if status.did_change().await? {
+        println!("3");
         insert_status_change(status.clone()).await?;
+        println!("4");
         send_email(status).await?;
     }
 
